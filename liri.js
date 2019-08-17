49626b45a471ea92dd852b419e4ad77019af8ba4
mp02 <molly_patterson@yahoo.com>
2019-08-17 10:02:37 -0500

    added for loop and method from office hours

--- liri.js
+++ liri.js
@@ -1,11 +1,39 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+var moment = require("moment");
+// this spotify or one above?
+var spotify = require("node-spotify-api");
+var command = process.argv[2];
+var params = [];
 
 //
 
+for (var i = 3; < process.argv.length; i++) {
+  params.push(process.argv[i]);
+  //everything inside this for loop
+}
+
+switch (command) {
+  case "movie-this":
+    console.log("");
+    break;
+  case "correct-comman":
+    console.log(" ");
+    break;
+
+    default: 
+    console.log("whoops..testput in something that dalls outside of these ");
+}
+
+console.log(params);
+console.log(command);
+
 //var bands = function(){
-    var axios = require("axios");
+  // call each one in a switch statement
+    
     var bandName = process.argv[2];
     var queryBandURL = "https://rest.bandsintown.com/artists/"+ bandName +"/events?app_id=codingbootcamp";
     axios.get(queryBandURL).then(
@@ -21,7 +49,7 @@ var keys = require("./keys.js");
 //how to make string again, so if band has 2 words it still goes through
 
 
-//var command = process.argv[2];
+
 //SPOTIFY STARTS HERE
 /*
 
